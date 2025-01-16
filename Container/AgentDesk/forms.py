29366b6f50fa0740d1c10f650cb1fb1061a6177e
JADER124 <jaderlopez186@gmail.com>
2025-01-16 16:31:29 -0500

    Login completo con diseño

--- Container/AgentDesk/forms.py
+++ Container/AgentDesk/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={
+                'class': ' w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all',
+                'placeholder': 'Enter your name',
+            }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+                'class': ' w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all',
+                'placeholder': 'Enter your password',
+            }))
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Container/AgentDesk/templates/**/*.html"],
+  content: ["./Container/AgentDesk/templates/**/*.html",
+    './Container/AgentDesk/forms.py',
+  ],
   theme: {
     extend: {},
   },
