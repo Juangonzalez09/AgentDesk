d0e0a320e3833df9a1ce1584d1b3cd898c5f3652
juangonzalez09 <gonzalezjuanm58@gmail.com>
2025-01-16 15:59:59 -0500

    Autenticacion fase 1 backend sin diseño

--- Container/AgentDesk/views.py
+++ Container/AgentDesk/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from .forms import LoginForm
 #Funcion index principal
 
 def IndexView(request):
-    return render(request,"index.html")
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username = cd['username'],
+                                password = cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return HttpResponse('Usuario Aunteticado')
+                else:
+                    return HttpResponse('El usuario no esta activo')
+            else:
+                return HttpResponse('La informacion del usuario no es correcta')
+    else:
+         form = LoginForm()     
+         return render(request,'index.html',{'form':form})  
